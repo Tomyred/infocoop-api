f672f944b89390f7672fa94169dc296c18d6a5c4
Tomyred <tomy.rojo.1@gmail.com>
2022-02-08 17:31:56 -0300

    color required

--- src/models/link.model.js
+++ src/models/link.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const schema = new mongoose.Schema(
     {
         title: { type: String, required: true },
-        color: { type: String, default: "#2196f3" },
+        color: { type: String, required: true, default: "#2196f3" },
         description: { type: String, required: false },
         url: { type: String, required: true },
     },
