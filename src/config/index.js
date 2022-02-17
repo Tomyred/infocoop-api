17ee7f6ffd938ef12d54eca1a02cd89f12d4a825
Tomyred <tomy.rojo.1@gmail.com>
2022-02-16 22:27:44 -0300

    removed: mongoDB uri

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,8 @@
 const config = {
-
-    BaseDomain: process.env.BACKEND_BASE_DOMAIN ? process.env.BACKEND_BASE_DOMAIN : "http://localhost:8080",
-    MongoDbConnection: process.env.BACKEND_DB_CONNECTION ?
-        // process.env.BACKEND_DB_CONNECTION : "mongodb+srv://senado-azure:senado@1@cluster0-ecned.azure.mongodb.net/desarrollo",
-        process.env.BACKEND_DB_CONNECTION : "mongodb+srv://tredDev:estac123456@estacionamiento.xp2fh.mongodb.net/estacionamiento?retryWrites=true&w=majority",
+    BaseDomain: process.env.BACKEND_BASE_DOMAIN
+        ? process.env.BACKEND_BASE_DOMAIN
+        : "http://localhost:8080",
+    MongoDbConnection: process.env.BACKEND_DB_CONNECTION,
 };
 
 export default config;
